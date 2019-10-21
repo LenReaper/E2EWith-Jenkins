688373636252de7f4bf986e7b6b228765f1f41ff
GP <guru@guru-PC>
2019-10-21 22:35:00 +0530

    Automation using Jenkins

---
+++ E2ESeleniumJenkins/src/org/automation/testing/sampletestcaseforjenkinsautomation/TC_002_SampleTestCase2.java
@@ -0,0 +1,21 @@
+package org.automation.testing.sampletestcaseforjenkinsautomation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002_SampleTestCase2 {
+
+	@Test
+	public void fblogin()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("uname1");
+		driver.findElement(By.id("pass")).sendKeys("password");
+		driver.quit();
+	}
+}
